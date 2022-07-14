1e37903f4109ae93b77dc8a6d7aab98488022243
aakashganga <108390882+aakashganga@users.noreply.github.com>
2022-07-14 10:09:38 +0530

    contact form

---
+++ mail.php
@@ -0,0 +1,19 @@
+<?php
+//get data from form  
+
+$name = $_POST['Name'];
+$email= $_POST['Email'];
+$message= $_POST['Message'];
+$subject= $_POST['Subject'];
+$number= $_POST['Phonenumber'];
+$to = "aakashganga.tungnath@gmail.com";
+$subject = "Mail from customer";
+$txt ="Name = ". $name . "\r\n  Email = " . $email  . "\r\n  Phone Number = " . $Phonenumber. "\r\n Subject =" . $subject . "\r\n Message =" . $message;
+$headers = "From: ndarudra1@gmail.com" . "\r\n" .
+"CC: aviraljnvrpg@gmail.com";
+if($email!=NULL){
+    mail($to,$subject,$txt,$headers);
+}
+//redirect
+header("Location:thankyou.html");
+?>
